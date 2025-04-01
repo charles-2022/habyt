b68e1aae061725a712e859fa1e88c8f7e5791a94
Charles <charlesmill65@gmail.com>
2025-04-02 00:12:17 +0800

    update all json

--- api/play.js
+++ api/play.js
@@ -6,7 +6,10 @@ export default (req, res) => {
   const filePath = path.join(process.cwd(), 'play.txt');
   const file = fs.readFileSync(filePath);
 
-  res.setHeader('Content-Type', 'application/octet-stream');
-  res.setHeader('Content-Disposition', 'attachment; filename=play.txt');
+  // Set Content-Type to 'text/plain' so it can be rendered in a browser as text
+  res.setHeader('Content-Type', 'text/plain');
+  
+  // Do not include Content-Disposition to prevent automatic download
   res.status(200).send(file);
 };
+
